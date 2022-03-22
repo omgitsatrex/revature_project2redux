b0b70e1d2d109ebef19d2d62272c7b8293f85a27
William Fuentes <fuenteswilliam0326@gmail.com>
2022-03-22 07:36:19 -0700

    making home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { IndexComponent } from './components/index/index.component';
+import { HomeComponent } from './components/home/home.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { BuyerPageComponent } from './components/buyer-page/buyer-page.component';
+import { HeaderComponent } from './components/header/header.component';
 
 
 @NgModule({
@@ -12,7 +17,11 @@ import { IndexComponent } from './components/index/index.component';
     AppComponent,
     LoginComponent,
     NavbarComponent,
-    IndexComponent
+    IndexComponent,
+    HomeComponent,
+    SidebarComponent,
+    BuyerPageComponent,
+    HeaderComponent
    
     
   ],
